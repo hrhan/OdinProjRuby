0ea7a6c2f43b91381abd493a77beee1feecf1b3a
Hee Ran Han <heeranh93@gmail.com>
2024-04-06 22:39:11 -0700

    fix

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,16 @@
 def caesar_cipher(string, shift)
     chars = string.split("")
-    return chars.map{ |char| caesar_cipher_char(char, shift).chr }.join("")
+    return chars.map{ |char| caesar_cipher_char(char, shift) }.join("")
 end
 
 def caesar_cipher_char(char, shift)
-    if char.ord >= 'a'.ord && char.ord <= 'z'.ord
-        return ((char.ord - 'a'.ord + shift) % 26) + 'a'.ord
-    elsif char.ord >= 'A'.ord && char.ord <= "Z".ord
-        return ((char.ord - 'A'.ord + shift) % 26) + 'A'.ord
-    else
-        return char
+    char_ord = char.ord
+    if char_ord >= 'a'.ord && char_ord <= 'z'.ord
+        char_ord = ((char_ord - 'a'.ord + shift) % 26) + 'a'.ord
+    elsif char_ord >= 'A'.ord && char_ord <= "Z".ord
+        char_ord = ((char_ord - 'A'.ord + shift) % 26) + 'A'.ord
     end
+    return char_ord.chr
 end
+
+puts caesar_cipher("What a string!", 5)
