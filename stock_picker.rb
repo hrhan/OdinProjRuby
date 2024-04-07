e269780e3f818d00621355a3a487ac64ee622eec
Hee Ran Han <heeranh93@gmail.com>
2024-04-07 00:55:29 -0700

    fix

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,21 +1,18 @@
 def stock_picker(prices)
-    min_price_indexes = [0]
+    min_price_index = 0
     max_price_index = 0
     max_profit = 0
+    ans = [0, 0]
     prices.each_with_index do | price, index |
-        min_price_index = min_price_indexes[-1]
         profit = price - prices[min_price_index]
         if price < prices[min_price_index]
-            min_price_indexes.push(index)
+            min_price_index = index
         elsif profit > max_profit
             max_price_index = index
             max_profit = profit
+            ans = [min_price_index, max_price_index]
         end
     end
 
-    while !min_price_indexes.empty? && min_price_indexes[-1] > max_price_index do
-        min_price_indexes.pop()
-    end
-
-    return [min_price_indexes[-1], max_price_index]
+    return ans
 end
